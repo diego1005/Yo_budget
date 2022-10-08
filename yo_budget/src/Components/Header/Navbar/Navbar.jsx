6d71676b571238890509ec35344816eaf888c783
diego1005 <dmaguilar82@gmail.com>
2022-10-08 19:14:17 -0300

    add component navbar, navicon, searchbar to front

--- yo_budget/src/Components/Header/Navbar/Navbar.jsx
+++ yo_budget/src/Components/Header/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar/SearchBar";
+import Navicon from "./Navicon/Navicon";
+
 
 function Navbar() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='navbar'>
+            <div className="nav-left">
+                <Link to="/"><i className="fa-solid fa-house"></i></Link>
+                <p>/</p>
+                <p className='left-title'>Yo_budget</p>
+            </div>
+            <div className="nav-right">
+                <SearchBar />
+                <Navicon />
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
---
+++ yo_budget/src/Components/Header/Navbar/Navicon/Navicon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Navicon.css";
+
+function Navicon() {
+  return (
+    <div>
+        <i className="fa-solid fa-user nav-icon"></i>
+        <i className="fa-solid fa-gear nav-icon"></i>
+        <i className="fa-solid fa-bell nav-icon"></i>
+    </div>
+  )
+}
+
+export default Navicon
