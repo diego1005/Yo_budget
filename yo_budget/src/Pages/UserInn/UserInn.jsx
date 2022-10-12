56aaff6c3b6e80e0fa96e4a91ba079656bd7e6a8
diego1005 <dmaguilar82@gmail.com>
2022-10-12 17:02:49 -0300

    implement states on user inn component to hide and show user log and sign boxes

--- yo_budget/src/Pages/UserInn/UserInn.jsx
+++ yo_budget/src/Pages/UserInn/UserInn.jsx
@@ -1,98 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./UserInn.css";
 
 function UserInn() {
 
-  /*
-  const showBox = (el) => {
-    let registerIcon = document.querySelector(".user-signin");
-    let register = document.querySelector("#register");
-    let loginIcon = document.querySelector(".user-login");
-    let login = document.querySelector("#login");
-    if (el === "r") {
-      if (loginIcon.style.display !== "none") {
-        login.style.display = "block";
-        loginIcon.style.display = "none";
-      }
-      register.style.display = "none"
-      registerIcon.style.display = "flex";
-    } else {
-      if (registerIcon.style.display !== "none") {
-        register.style.display = "block";
-        registerIcon.style.display = "none";
-      }
-      login.style.display = "none"
-      loginIcon.style.display = "flex";
+  const [registerBox, setRegisterBox] = useState(false)
+  const [registerBtn, setRegisterBtn] = useState(true)
+  const [loginBox, setLoginBox] = useState(false)
+  const [loginBtn, setLoginBtn] = useState(true)
+
+  const showHideReg = () => {
+    setRegisterBox((prevState) => !prevState);
+    setRegisterBtn((prevState) => !prevState);
+    if (loginBox) {
+      setLoginBox((prevState) => !prevState)
+      setLoginBtn((prevState) => !prevState);
     }
   }
-  */
+  
+  const showHideLog = () => {
+    setLoginBox((prevState) => !prevState);
+    setLoginBtn((prevState) => !prevState);
+    if (registerBox) {
+      setRegisterBox((prevState) => !prevState)
+      setRegisterBtn((prevState) => !prevState);
+    }
+  }
 
   return (
     <div className='userinn'>
-      <div id='register'>
-        <i className="fa-solid fa-user-plus userinn-icon register"></i>
-      </div>
-      <div id='login'>
-        <i className="fa-solid fa-user-check userinn-icon login"></i>
-      </div>
-      <div className="user-signin">
-        <i className="fa-solid fa-user-plus userinn-icon register"></i>
-        <div className="userinn-title-box">
-          <hr />
-          <h3 className='inn-title'>Register</h3>
-          <hr />
+      {
+        registerBtn &&
+        <div>
+          <i className="fa-solid fa-user-plus userinn-icon register" onClick={showHideReg}></i>
         </div>
-        <form className='signin-form'>
-          <div className="form-ctr">
-            <i className="fa-solid fa-user"></i>
-            <input type="text" placeholder='Name' />
+      }
+      {
+        registerBox &&
+        <div className="user-signin">
+          <i className="fa-solid fa-user-plus userinn-icon register"></i>
+          <div className="userinn-title-box">
+            <hr />
+            <h3 className='inn-title'>Register</h3>
+            <hr />
           </div>
-          <div className="form-ctr">
-            <i className="fa-solid fa-user"></i>
-            <input type="text" placeholder='Lastname' />
-          </div>
-          <div className="form-ctr">
-            <i className="fa-solid fa-envelope"></i>
-            <input type="email" placeholder='Email' />
-          </div>
-          <div className="form-ctr">
-            <i className="fa-solid fa-lock"></i>
-            <input type="password" placeholder='Password' />
-          </div>
-          <div className="form-ctr">
-            <i className="fa-solid fa-lock"></i>
-            <input type="password" placeholder='Confirm Password' />
-          </div>
-          <div className="form-ctr">
-            <i className="fa-solid fa-image"></i>
-            <input type="text" placeholder='image' />
-          </div>
-          <div className="form-ctr">
-            <input className='btn-register' type="submit" value="Register" />
-          </div>
-        </form>
-      </div>
-      <div className="user-login">
-        <i className="fa-solid fa-user-check userinn-icon login"></i>
-        <div className="userinn-title-box">
-          <hr />
-          <h3 className='inn-title'>Login</h3>
-          <hr />
+          <form className='signin-form'>
+            <div className="form-ctr">
+              <i className="fa-solid fa-user"></i>
+              <input type="text" placeholder='Name' />
+            </div>
+            <div className="form-ctr">
+              <i className="fa-solid fa-user"></i>
+              <input type="text" placeholder='Lastname' />
+            </div>
+            <div className="form-ctr">
+              <i className="fa-solid fa-envelope"></i>
+              <input type="email" placeholder='Email' />
+            </div>
+            <div className="form-ctr">
+              <i className="fa-solid fa-lock"></i>
+              <input type="password" placeholder='Password' />
+            </div>
+            <div className="form-ctr">
+              <i className="fa-solid fa-lock"></i>
+              <input type="password" placeholder='Confirm Password' />
+            </div>
+            <div className="form-ctr">
+              <i className="fa-solid fa-image"></i>
+              <input type="text" placeholder='image' />
+            </div>
+            <div className="form-ctr">
+              <input className='btn-register' type="submit" value="Register" />
+            </div>
+          </form>
         </div>
-        <form className='login-form'>
-          <div className="form-ctr">
-            <i className="fa-solid fa-envelope"></i>
-            <input type="email" placeholder='Email' />
+      }
+      {
+        loginBtn &&
+        <div>
+          <i className="fa-solid fa-user-check userinn-icon login" onClick={showHideLog}></i>
+        </div>
+      }
+      {
+        loginBox &&
+        <div className="user-login">
+          <i className="fa-solid fa-user-check userinn-icon login"></i>
+          <div className="userinn-title-box">
+            <hr />
+            <h3 className='inn-title'>Login</h3>
+            <hr />
           </div>
-          <div className="form-ctr">
-            <i className="fa-solid fa-lock"></i>
-            <input type="password" placeholder='Password' />
-          </div>
-          <div className="form-ctr">
-            <input className='btn-login' type="submit" value="Login" />
-          </div>
-        </form>
-      </div>
+          <form className='login-form'>
+            <div className="form-ctr">
+              <i className="fa-solid fa-envelope"></i>
+              <input type="email" placeholder='Email' />
+            </div>
+            <div className="form-ctr">
+              <i className="fa-solid fa-lock"></i>
+              <input type="password" placeholder='Password' />
+            </div>
+            <div className="form-ctr">
+              <input className='btn-login' type="submit" value="Login" />
+            </div>
+          </form>
+        </div>
+      }
     </div>
   )
 }
